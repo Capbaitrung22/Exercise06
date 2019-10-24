5e71ab9807eb8441b31907514b758fc097d5cad1
TrungThanhNhan <capbaitrung22@gmail.com>
2019-10-24 15:27:24 +0700

    Bailam

---
+++ Exercise06.2/Exercise06.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Trung", "Thanh", "Nhan" };
+
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " ");
+        }
+    }
+}
---
+++ Exercise06.2/Exercise06.2/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06._2
+{
+    public class Stack
+    {
+            string[] Buffer;
+            int Top;
+
+            public Stack(int size)
+            {
+                Buffer = new string[size];
+                Top = -1;
+        
+            }
+        /// <summary>
+        /// Initilize Stack with Buffer
+        /// </summary>
+        public Stack()
+        {
+            Buffer = new string[1000];
+        }
+        public void Push(string value)
+        {
+            Buffer[Top + 1] = value;
+            Top = Top + 1;
+        }
+        public string Pop()
+        {
+            string value = Buffer[Top];
+            Top = Top - 1;
+            return value;
+        }
+        public string peek()
+        {
+            return Buffer[Top];
+        }
+
+        public int Count()
+        {
+            return Top + 1;
+        }
+
+        public void Clear()
+        {
+            Top = -1;
+        }
+    
+    }
+}
